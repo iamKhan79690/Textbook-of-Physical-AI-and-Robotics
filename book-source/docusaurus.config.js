d7ca222f84a1051f1bf6b7f7ab8dde01fb0ad073
Anas Ur Rehman <145249950+iamKhan79690@users.noreply.github.com>
2025-12-01 08:22:07 -0800

    Configure GitHub Pages deployment

--- book-source/docusaurus.config.js
+++ book-source/docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://iamKhan79690.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Textbook-of-Physical-AI-and-Robotics/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'your-org', // Usually your GitHub org/user name.
-  projectName: 'ai-book', // Usually your repo name.
+  organizationName: 'iamKhan79690', // Usually your GitHub org/user name.
+  projectName: 'Textbook-of-Physical-AI-and-Robotics', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
